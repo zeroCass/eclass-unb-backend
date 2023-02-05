9ec2531793ad02387993f7ca97d34d0c1f1b4e23
jessica8melo <jessica8melo@gmail.com>
2023-02-04 21:54:16 -0300

    ✨ user model created

--- e_class/models.py
+++ e_class/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+
+    class Type(models.IntegerChoices):
+        Admins = 1
+        Teachers = 2
+        Students = 3
+
+    name = models.CharField(max_length=45, blank=False)
+    email = models.EmailField(max_length=254, blank=False)
+    password = models.CharField(max_length=15, blank=False)
+    cpf = models.CharField(max_length=11, blank=True)
+    userType = models.IntegerField(choices=Type.choices, blank=False)
+
+    def __str__(self):
+        return self.name
