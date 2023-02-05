27d92944e9a0b811fa6b6b4138b0569a4febd651
jessica8melo <jessica8melo@gmail.com>
2023-02-04 21:53:18 -0300

    🧪 testing users failed

---
+++ e_class/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from e_class.models import Users
+
+class UsersTestCase(TestCase):
+    def setUp(self):
+        Users.objects.create(name='test',
+                             email='test@test',
+                             password='123456',
+                             userType=1)
+        
+    def test_user_name_blank(self):
+        """Testa se o nome de usuário está vazio"""
+        user = Users.objects.create(email='test@test',
+                                    password='123456',
+                                    userType=1)
+        self.assertEqual(user.name, '')
+
+
+    def test_user_email_blank(self):
+        """Testa se o email de usuário está vazio"""
+        user = Users.objects.create(name='test',
+                                    password='123456',
+                                    userType=1)
+        self.assertEqual(user.email, ''):
+            
+
+    def test_user_password_blank(self):
+        """Testa se a senha de usuário está vazia"""
+        user = Users.objects.create(name='test',
+                                    email='test@test',
+                                    userType=1)
+        self.assertEqual(user.password, '')
