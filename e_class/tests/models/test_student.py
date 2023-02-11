91fca8b0c30f2851b737ae4d21c9fded2637f5ab
jessica8melo <jessica8melo@gmail.com>
2023-02-11 19:57:47 -0300

    ✅ student model tests passed

--- e_class/tests/models/test_student.py
+++ e_class/tests/models/test_student.py
@@ -3,21 +3,20 @@ from e_class.models import Student #type:ignore
 
 """testando tabela estudante"""
 class StudentModelTest(TestCase):
-    def setUpStudents(self):
-        User = User.objects.create(name='test',
-                             email='test@test',
-                             password='123456',
-                             userType=1,
-                             id=5)
-        
-        Student.objects.create(Users_registrationID=User)
+    def setUp(self):
+        Student.objects.create(name='aluno1',
+                             email='test1@test',
+                             cpf='00000000000',
+                             userType=3)
+        Student.objects.create(name='aluno2',
+                             email='test2@test',
+                             cpf='11111111111',
+                             userType=3)
 
-    def test_Student_id_fk(self):
-        """Testa se a chave estrangeira do estudante 
-        é igual a chave primária de usuário"""
-        User = User.objects.create(name='test',
-                             email='test@test',
-                             password='123456',
-                             userType=1)
-        Student = Student.objects.create(Users_registrationID=User)
-        self.assertEqual(Student.Users_registrationID.id, 2)
+
+    def test_student_unicode(self):
+        """Testa se o nome do aluno é o esperado"""
+        student1 = Student.objects.get(name='aluno1')
+        student2 = Student.objects.get(name='aluno2')
+        self.assertEqual(student1.__unicode__(), 'aluno1')
+        self.assertEqual(student2.__unicode__(), 'aluno2')
