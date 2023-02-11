a4dd8077b4508fda3370dfc95046b4cdecd61af7
pafev <paulofvilarimda@gmail.com>
2023-02-11 00:57:26 -0300

    model Classes e relação com Subject e Students

--- e_class/models.py
+++ e_class/models.py
@@ -39,7 +39,8 @@ class Student(User):
 
 
 class Subject(models.Model):
-    admin_id = models.ForeignKey(Admin, on_delete=models.PROTECT)
+    admin = models.ForeignKey(Admin, on_delete=models.PROTECT)
+    teachers = models.ManyToManyField(Teacher)
     course = models.CharField(max_length=45, unique=True)
     description = models.TextField(max_length=200)
 
@@ -48,3 +49,27 @@ class Subject(models.Model):
 
     class Meta:
         ordering = ["course"]
+
+class Classes(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student, through="Students_has_Classes")
+    name = models.CharField(max_length=15)
+    size = models.IntegerField()
+    startTime = models.DateField()
+    endTime = models.DateField()
+    period = models.IntegerField()
+    password = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+class Students_has_Classes(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    classes = models.ForeignKey(Classes, on_delete=models.CASCADE)
+    registerDate = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["registerDate"]
