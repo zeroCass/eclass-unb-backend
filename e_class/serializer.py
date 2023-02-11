67f21a0c16576033770e5529be10d13129e9efae
Luana Cruz <luanacruzsilva2015@gmail.com>
2023-02-11 18:54:56 -0300

    serializer create

--- e_class/serializer.py
+++ e_class/serializer.py
@@ -27,3 +27,23 @@ class ClassesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classes
         fields = ['id', 'subject', 'students', 'teachers', 'name', 'size', 'startTime', 'endTime', 'period', 'password', 'createdAt']
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['is_visibility', 'statement', 'students', 'teacher']
+
+class MultipleQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MultipleQuestion
+        fields = ['id', 'option1', 'option2', 'option3', 'option4', 'answer', 'is_visibility', 'statement', 'students', 'teacher']
+
+class DiscursiveQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DiscursiveQuestion
+        fields = ['id', 'answer', 'is_visibility', 'statement', 'students', 'teacher']
+
+class ExamsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exams
+        fields = ['id', 'startAt', 'endedAt', 'isVisible', 'score', 'multipleQuestions', 'discursiveQuestions', 'students', 'teacher']
