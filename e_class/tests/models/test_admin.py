648d82045765fceea2ccd77316bfc7e8bef757c8
jessica8melo <jessica8melo@gmail.com>
2023-02-11 19:57:37 -0300

    ✅ admin model tests passed

--- e_class/tests/models/test_admin.py
+++ e_class/tests/models/test_admin.py
@@ -3,21 +3,22 @@ from e_class.models import Admin #type:ignore
 
 """testando tabela administrador"""
 class AdminModelTest(TestCase):  
-    def setUpAdmins(self):
-        User = User.objects.create(name='test',
-                             email='test@test',
-                             password='123456',
-                             userType=1,
-                             id=5)
-        
-        Admin.objects.create(Users_registrationID=User)
-
-    def test_Admins_id_fk(self):
-        """Testa se a chave estrangeira do administrador
-        é igual a chave primária de usuário"""
-        User = User.objects.create(name='test',
-                             email='test@test',
-                             password='123456',
+    def setUp(self):
+        Admin.objects.create(name='administrador1',
+                             email='test1@test',
+                             cpf='00000000000',
                              userType=1)
-        Admin = Admin.objects.create(Users_registrationID=User)
-        self.assertEqual(Admin.Users_registrationID.id, 2)
+        Admin.objects.create(name='administrador2',
+                             email='test2@test',
+                             cpf='11111111111',
+                             userType=1)
+
+    def test_admin_unicode(self):
+        """Testa se o nome do administrador é o esperado"""
+        admin1 = Admin.objects.get(name='administrador1')
+        admin2 = Admin.objects.get(name='administrador2')
+        self.assertEqual(admin1.__unicode__(), 'administrador1')
+        self.assertEqual(admin2.__unicode__(), 'administrador2')
+
+    
+    
