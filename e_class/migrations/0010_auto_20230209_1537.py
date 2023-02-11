4abaf98cf5c81da9737191c540c0e1afda8f6b8d
jessica8melo <jessica8melo@gmail.com>
2023-02-11 19:56:57 -0300

    deleting RemoveField migrations

--- e_class/migrations/0010_auto_20230209_1537.py
+++ e_class/migrations/0010_auto_20230209_1537.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='students',
-            name='id',
-        ),
         migrations.AlterField(
             model_name='students',
             name='Users_registrationID',
