c658b7249024cf79e07ea08a460c245481d52150
pafev <paulofvilarimda@gmail.com>
2023-02-12 02:51:03 -0300

    view for teachers and admins

--- e_class/urls.py
+++ e_class/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include #type:ignore
 from .views.view_students import *
+from .views.view_teachers import *
+from .views.view_admins import *
 # from rest_framework.routers import DefaultRouter #type:ignore
 
 urlpatterns = [
     path('students/', studentsList.as_view()),
     path('students/<int:pk>/', studentOne.as_view()),
+    path('teachers/', teachersList.as_view()),
+    path('teachers/<int:pk>/', teacherOne.as_view()),
+    path('admins/', adminsList.as_view()),
+    path('admins/<int:pk>/', adminOne.as_view()),
 ]
