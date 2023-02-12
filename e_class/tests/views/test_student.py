ff8c6cb0d81035ded6488a945263ef8a3a785a42
jessica8melo <jessica8melo@gmail.com>
2023-02-12 03:39:36 -0300

    testing students views

--- e_class/tests/views/test_student.py
+++ e_class/tests/views/test_student.py
@@ -1,17 +1,21 @@
 import json
 from django.test import TestCase #type:ignore
-from e_class.models import Student #type:ignore
-from e_class.serializer import StudentsSerializer
+from ...models import Student #type:ignore
+from ...serializer import StudentsSerializer, StudentsSerializerEDIT
+from ...views.view_students import studentOne, studentsList
+from ...urls import *
 from django.urls import reverse #type: ignore
 from rest_framework import status #type: ignore
 
-user = Student()
+users = studentsList()
+user = studentOne()
 class StudentViewTest(TestCase):
     """ Teste para os métodos de alunos """
 
     def setUp(self):
         self.aluno1 = Student.objects.create(name='aluno1',
                              email='test1@test',
+                             password='123456',
                              cpf='00000000000',
                              userType=3)
         self.aluno2 = Student.objects.create(name='aluno2',
@@ -39,48 +43,40 @@ class StudentViewTest(TestCase):
             'userType': 3
         }
 
-    def test_get_all_students(self):
+    def test_get_studentsList(self):
         """ Teste para buscar todos alunos """
-        response = user.get(reverse('get_post_students'))
+        response = users.get()
         students = Student.objects.all()
         serializer = StudentsSerializer(students, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_valid_single_student(self):
+    def test_get_valid_studentOne(self):
         """ Teste válido para buscar um aluno """
-        response = user.get(
-            reverse('get_delete_update_student', kwargs={'pk': self.aluno3.pk}))
+        response = user.get(self.aluno3.pk)
         student = Student.objects.get(pk=self.aluno3.pk)
         serializer = StudentsSerializer(student)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_invalid_single_student(self):
+    def test_get_invalid_studentOne(self):
         """ Teste inválido para buscar um aluno """
-        response = user.get(
-            reverse('get_delete_update_student', kwargs={'pk': 30}))
+        response = user.getObject(30)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_create_valid_student(self):
+    def test_create_valid_studentList(self):
         """ Teste válido para criar novo aluno """
-        response = user.post(
-            reverse('get_post_students'),
-            data=json.dumps(self.valid_payload),
-            content_type='application/json'
-        )
+        response = users.post(self.valid_payload.items)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_invalid_student(self):
+    def test_create_invalid_studentList(self):
         """ Teste inválido para criar novo aluno """
-        response = user.post(
-            reverse('get_post_students'),
-            data=json.dumps(self.invalid_payload),
+        response = user.post(data=json.dumps(self.invalid_payload),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_valid_update_student(self):
+    def test_valid_update_studentOne(self):
         """ Teste válido para atualizar um aluno já existente """
         response = user.put(
             reverse('get_delete_update_student', kwargs={'pk': self.aluno2.pk}),
@@ -89,7 +85,7 @@ class StudentViewTest(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_invalid_update_student(self):
+    def test_invalid_update_studentOne(self):
         """ Teste inválido para atualizar um aluno já existente """
         response = user.put(
             reverse('get_delete_update_student', kwargs={'pk': self.aluno2.pk}),
@@ -97,14 +93,12 @@ class StudentViewTest(TestCase):
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_valid_delete_student(self):
+    def test_valid_delete_studentOne(self):
         """ Teste válido para apagar um aluno já existente """
-        response = user.delete(
-            reverse('get_delete_update_student', kwargs={'pk': self.aluno2.pk}))
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = user.delete(self.aluno2.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_invalid_delete_student(self):
+    def test_invalid_delete_studentOne(self):
         """ Teste inválido para apagar um aluno já existente """
-        response = user.delete(
-            reverse('get_delete_update_student', kwargs={'pk': 30}))
+        response = user.delete(30)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
