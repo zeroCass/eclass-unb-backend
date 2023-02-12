df0c97ce33a65ae695777dec46391c8497f77760
jessica8melo <jessica8melo@gmail.com>
2023-02-12 01:46:57 -0300

    teacher view tests updated

--- e_class/tests/views/test_teacher.py
+++ e_class/tests/views/test_teacher.py
@@ -6,36 +6,8 @@ from django.urls import reverse
 from rest_framework import status
 
 user = Teacher()
-class GetAllTeacherTest(TestCase):
-    """ Teste para buscar todos professores """
-
-    def setUp(self):
-        Teacher.objects.create(name='professor1',
-                             email='test1@test',
-                             cpf='00000000000',
-                             userType=3)
-        Teacher.objects.create(name='professor2',
-                             email='test2@test',
-                             cpf='11111111111',
-                             userType=3)
-        Teacher.objects.create(name='professor3',
-                             email='test3@test',
-                             cpf='22222222222',
-                             userType=3)
-        Teacher.objects.create(name='professor4',
-                             email='test4@test',
-                             cpf='33333333333',
-                             userType=3)
-
-    def test_get_all_teachers(self):
-        response = user.get(reverse('get_post_teachers'))
-        teachers = Teacher.objects.all()
-        serializer = TeachersSerializer(teachers, many=True)
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-class GetSingleTeacherTest(TestCase):
-    """ Teste para buscar um professor """
+class TeacherViewTest(TestCase):
+    """ Teste para os métodos de professores """
 
     def setUp(self):
         self.professor1 = Teacher.objects.create(name='professor1',
@@ -54,24 +26,6 @@ class GetSingleTeacherTest(TestCase):
                              email='test4@test',
                              cpf='33333333333',
                              userType=3)
-
-    def test_get_valid_single_teacher(self):
-        response = user.get(
-            reverse('get_delete_update_teacher', kwargs={'pk': self.professor3.pk}))
-        teacher = Teacher.objects.get(pk=self.professor3.pk)
-        serializer = TeachersSerializer(teacher)
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_invalid_single_teacher(self):
-        response = user.get(
-            reverse('get_delete_update_teacher', kwargs={'pk': 30}))
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-class CreateNewTeacherTest(TestCase):
-    """ Teste para criar novo professor """
-
-    def setUp(self):
         self.valid_payload = {
             'name': 'professor1',
             'email': 'teste1@test',
@@ -85,7 +39,31 @@ class CreateNewTeacherTest(TestCase):
             'userType': 3
         }
 
+    def test_get_all_teachers(self):
+        """ Teste para buscar todos professores """
+        response = user.get(reverse('get_post_teachers'))
+        teachers = Teacher.objects.all()
+        serializer = TeachersSerializer(teachers, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_valid_single_teacher(self):
+        """ Teste válido para buscar um professor """
+        response = user.get(
+            reverse('get_delete_update_teacher', kwargs={'pk': self.professor3.pk}))
+        teacher = Teacher.objects.get(pk=self.professor3.pk)
+        serializer = TeachersSerializer(teacher)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_teacher(self):
+        """ Teste inválido para buscar um professor """
+        response = user.get(
+            reverse('get_delete_update_teacher', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_create_valid_teacher(self):
+        """ Teste válido para criar novo professor """
         response = user.post(
             reverse('get_post_teachers'),
             data=json.dumps(self.valid_payload),
@@ -94,6 +72,7 @@ class CreateNewTeacherTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_invalid_teacher(self):
+        """ Teste inválido para criar novo professor """
         response = user.post(
             reverse('get_post_teachers'),
             data=json.dumps(self.invalid_payload),
@@ -101,28 +80,8 @@ class CreateNewTeacherTest(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-class UpdateSingleTeacherTest(TestCase):
-    """ Teste para atualizar um professor já existente """
-
-    def setUp(self):
-        self.professor1 = Teacher.objects.create(
-            name='professor1', email='test1@test', cpf='00000000000', userType=3)
-        self.professor2 = Teacher.objects.create(
-            name='professor2', email='test2@test', cpf='11111111111', userType=3)
-        self.valid_payload = {
-            'name': 'professor2.0',
-            'email': 'test2.0@test',
-            'cpf': '11111111111',
-            'userType': 3
-        }
-        self.invalid_payload = {
-            'name': '',
-            'email': 'test2.0@test',
-            'cpf': '11111111111',
-            'userType': 3
-        }
-
     def test_valid_update_teacher(self):
+        """ Teste válido para atualizar um professor já existente """
         response = user.put(
             reverse('get_delete_update_teacher', kwargs={'pk': self.professor2.pk}),
             data=json.dumps(self.valid_payload),
@@ -131,27 +90,21 @@ class UpdateSingleTeacherTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_invalid_update_teacher(self):
+        """ Teste inválido para atualizar um professor já existente """
         response = user.put(
             reverse('get_delete_update_teacher', kwargs={'pk': self.professor2.pk}),
             data=json.dumps(self.invalid_payload),
             content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-class DeleteSingleTeacherTest(TestCase):
-    """ Teste para apagar um professor já existente """
-
-    def setUp(self):
-        self.professor1 = Teacher.objects.create(
-            name='professor1', email='test1@test', cpf='00000000000', userType=3)
-        self.professor2 = Teacher.objects.create(
-            name='professor2', email='test2@test', cpf='11111111111', userType=3)
-
     def test_valid_delete_teacher(self):
+        """ Teste válido para apagar um professor já existente """
         response = user.delete(
             reverse('get_delete_update_teacher', kwargs={'pk': self.professor2.pk}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_invalid_delete_teacher(self):
+        """ Teste inválido para apagar um professor já existente """
         response = user.delete(
             reverse('get_delete_update_teacher', kwargs={'pk': 30}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
